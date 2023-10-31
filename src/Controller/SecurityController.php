5a362588572916192ce5e369e6cbb0e3e4affec5
Mamadou Aliou Diallo <alioukahere@gmail.com>
2023-10-31 18:49:31 +0000

    authenticate user

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use LogicException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login')]
+    public function login(#[CurrentUser()] ?User $user): Response
+    {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'Authentication failed',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json([
+            'message' => 'Authentication successful',
+        ]);
+    }
+}
